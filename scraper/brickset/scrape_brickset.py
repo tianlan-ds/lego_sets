41f455d81a806eb8bd62a8a96a6050d7cdbe6851
tianlan-ds <tianlan.email@gmail.com>
2019-07-14 12:28:11 -0500

    added logic to scrape a theme and a list of themes

--- scraper/brickset/scrape_brickset.py
+++ scraper/brickset/scrape_brickset.py
@@ -1,10 +1,13 @@
 from typing import List, Dict, Any
+import logging
 
 from bs4 import BeautifulSoup
 
 from scraper.browser_driver import Browser
 from scraper.brickset.set_scraper import SetScraper
 from scraper.scraper_constants import BrickSet
+
+logger = logging.getLogger(__name__)
 
 
 def get_href_link(page_url: str) -> List[str]:
@@ -35,8 +38,57 @@ def scrape_one_page(page_url: str) -> List[Dict[str, Any]]:
     set_info_from_one_page = []
     for href in href_list:
         set_url = start_url + href
-        set_scraper = SetScraper(set_url, sleep_time=BrickSet.sleep_time)
+        set_scraper = SetScraper(set_url, sleep_time=BrickSet.SLEEP_TIME)
         set_info = set_scraper.set_data
-        set_info_from_one_page.append(set_info)
+        set_number = set_info['set_number']
+        set_name = set_info['set_name']
+        logger.info('done scraping set {} {}'.format(set_number, set_name))
+        # if the set name has {} in it, it means the set is just a place holder and it is not released yet. Then log
+        # this information and skip saving the data
+        if '{' in set_name and '}' in set_name:
+            logger.info('not saving set {} {}, as it is not released yet'.format(set_number, set_name))
+        else:
+            set_info_from_one_page.append(set_info)
 
     return set_info_from_one_page
+
+
+def scrape_one_theme(theme_name: str) -> List[Dict[str, Any]]:
+    """
+    Given a theme name, loop through all pages, and scrape each set data on each page.
+
+    :param theme_name: theme name
+    :return: all the set data for the given theme name
+    """
+    logger.info('scraping theme {}'.format(theme_name))
+    start_url = 'https://brickset.com/sets/theme-{}/page-'.format(theme_name)
+    page_num = 1
+    all_set_data = []
+    while True:
+        page_url = start_url + str(page_num)
+        logger.info('scraping page {}, url: {}'.format(str(page_num), page_url))
+        set_data_from_one_page = scrape_one_page(page_url)
+        if len(set_data_from_one_page) > 0:
+            all_set_data.extend(set_data_from_one_page)
+            page_num += 1
+        else:
+            break
+
+    logger.info('done scraping theme {}'.format(theme_name))
+
+    return all_set_data
+
+
+def scrape_all_theme(theme_list: List[str]) -> List[Dict[str, Any]]:
+    """
+    Loop through the theme list and scrape data for all the set within each theme.
+
+    :param theme_list: a list of theme to scrape
+    :return: all the set data for the entire theme list
+    """
+    all_set_data = []
+    for theme_name in theme_list:
+        one_theme_data = scrape_one_theme(theme_name)
+        all_set_data.extend(one_theme_data)
+
+    return all_set_data
