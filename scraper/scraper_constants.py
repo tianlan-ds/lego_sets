469559dfc4f3eb08f6dea07d0f3f7ba673f5be2b
tianlan-ds <tianlan.email@gmail.com>
2019-07-14 12:28:33 -0500

    added a list of themes to scrape

--- scraper/scraper_constants.py
+++ scraper/scraper_constants.py
@@ -2,4 +2,6 @@ import os
 
 
 class BrickSet(object):
-    sleep_time = int(os.environ.get("BRICKSET_SLEEP_TIME", "1"))
+    SLEEP_TIME = int(os.environ.get("BRICKSET_SLEEP_TIME", "1"))
+
+    THEME_LIST = ['Creator-Expert', 'Architecture', 'Ideas', 'Star-Wars', 'Technic']
